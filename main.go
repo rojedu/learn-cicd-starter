d5323e890fee8e527ce232d7ea559a888d58c47c
rojedu <mlukacev@inet.hr>
2026-09-22 20:55:25 +0200

    Ispravljene greske gosec

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,11 +91,12 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 10 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving on port: %s\n", strings.ReplaceAll(strings.ReplaceAll(port, "\n", ""), "\r", ""))
 	log.Fatal(srv.ListenAndServe())
 }
 
